fc4ed7cc51093cc351f8ab2db169978b7ba12410
Brandon Chen <brandonchenjiacheng@gmail.com>
2017-05-10 12:33:06 -0700

    added 'make-them-say.py' so user can get a full audio clip from inputted text

---
+++ make-them-say.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+import sys
+import os
+from pydub import AudioSegment
+
+# def find(name, path):
+#     for root, dirs, files in os.walk(path):
+#         if name in files:
+#             return os.path.join(root, name)
+
+def main(argv) :
+	if(len(sys.argv) != 2): #TODO: change this to allow input transcript
+		print('Usage: make-them-say.py statement')
+		sys.exit(2)
+
+	# basename = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+	phrase = os.path.splitext(sys.argv[1])[0]
+	words = phrase.split()
+
+	combined_audio = AudioSegment.silent(duration=0) 
+
+	for word in words:
+		# clip = find(word, '/clips')
+		audio = AudioSegment.from_wav("clips/" + word + ".wav")
+		combined_audio += audio #+ AudioSegment.silent(duration=0.75) 
+
+	combined_audio.export("output/they-say.wav", format="wav")
+
+if __name__ == "__main__" :
+	main(sys.argv[1:])
