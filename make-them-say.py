68619e24dd23f990f5568d2fd5bbc39587717c4e
Brandon Chen <brandonchenjiacheng@gmail.com>
2017-05-30 17:35:42 -0700

    updated make-them-say.py to current version

--- make-them-say.py
+++ make-them-say.py
@@ -9,12 +9,9 @@ from moviepy.config import get_setting
 def concat(clips):
 		"""
 	Concatenates the the input clips into an output video
-
 	Parameters
     --------------------
         clips			-- 	Array of clip names excluding filetype as we assume them to be .mp4
-
-
     Returns
     --------------------
         returns nothing but creates an output video
@@ -57,10 +54,6 @@ def concat(clips):
 #         if name in files:
 #             return os.path.join(root, name)
 
-def match_target_amplitude(sound, target_dBFS):
-    change_in_dBFS = target_dBFS - sound.dBFS
-    return sound.apply_gain(change_in_dBFS)
-
 def main(argv) :
 	if(len(sys.argv) != 2): 
 		print('Usage: make-them-say.py statement')
@@ -68,26 +61,8 @@ def main(argv) :
 
 	phrase = os.path.splitext(sys.argv[1])[0]
 	words = phrase.split()
-	combined_audio = AudioSegment.silent(duration=0)
-	
-	for word in words:
-		if word == '-': #dashes seperated by space will insert extra silences
-			audio = AudioSegment.silent(duration=100)
-		else:
-			audio = AudioSegment.from_wav("clips/" + word + "/1.wav")
 
-		normalized_audio = match_target_amplitude(audio, -15.0)
-		combined_audio += normalized_audio
-
-		# crossed_audio = audio#.fade_in(len(audio)/150)#.fade_out(len(audio)/5)
-		print(word + " - " + str(len(audio)))
-		# combined_audio += audio
-
-		# combined_audio += crossed_audio # + AudioSegment.silent(duration=35) 
-		# combined_audio.append(audio, crossfade=(len(audio)/2))
-		# combined_audio += AudioSegment.silent(duration=35) 
-
-	combined_audio.export("output/they-say.wav", format="wav")
+	concat(words)
 	
 if __name__ == "__main__" :
 	main(sys.argv[1:])
