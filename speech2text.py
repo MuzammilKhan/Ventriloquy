cfa852d64af6024b7a8f7a45f5ed4434c7f01aed
Brandon Chen <brandonchenjiacheng@gmail.com>
2017-06-03 18:27:10 -0700

    lots of bug fixes to multi-threading.

--- speech2text.py
+++ speech2text.py
@@ -14,8 +14,8 @@ import threading
 ######################################################################
 # global variables
 ######################################################################
-#Watson Speech To Text API login info
-stt = SpeechToTextV1(
+
+stt = SpeechToTextV1(	#Watson Speech To Text API login info
 	username = conf.username, 
 	password = conf.password,
 	x_watson_learning_opt_out=True
@@ -24,28 +24,24 @@ stt = SpeechToTextV1(
 num_threads = 3
 
 basename = ""
-file_ext = ""
 person = ""
-input_video = ""
-offset_amt = 0
-
 
 ######################################################################
-# functions
+# classes and functions
 ######################################################################
 class myThread (threading.Thread): #used this as guide: https://www.tutorialspoint.com/python3/python_multithreading.htm
-	def __init__(self, threadID, audio):
+	def __init__(self, threadID, audio, start_time):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.audio = audio
+		self.start_time = start_time
 
 	def run(self):
-		print ("Starting " + self.name)
-		endtime=len(self.audio)
-		increment_by= 60000
-		for i in range(self.threadID, endtime, increment_by):
-			if(i + 60000 > endtime):
-				audio_chunk = self.audio[i:endtime]
+		end_time = len(self.audio)
+		increment_by = 60000
+		for i in range(self.threadID, end_time, increment_by):
+			if(i + 60000 > end_time):
+				audio_chunk = self.audio[i:end_time]
 			else:
 				audio_chunk = self.audio[i:i+60000]
 			audio_chunk.export("workspace/" + str(self.threadID) + "_" + str(i) + ".wav", format="wav")
@@ -54,40 +50,56 @@ class myThread (threading.Thread): #used this as guide: https://www.tutorialspoi
 			with open("workspace/" + str(self.threadID) + "_" + str(i) + ".wav", 'rb') as audio:
 				stt.models()
 				stt.get_model('en-US_BroadbandModel')
-				stt_result = stt.recognize(
-					audio, content_type='audio/wav', timestamps=True, word_confidence=True, continuous=True, profanity_filter=False,
-					word_alternatives_threshold=0.0
-				)#the parameters above can be altered to effect the output of the api
+
+				#these parameters above can be altered to effect the output of the api
+				stt_result = stt.recognize( audio, content_type='audio/wav', 
+													timestamps=True, 
+													word_confidence=True, 
+													continuous=True, 
+													profanity_filter=False,
+													word_alternatives_threshold=0.0 )
 
 				#dump response to a json file if we want to check it later then open it
 				with open('speech-snippets/' + basename + '_' + str(i) + '.json', 'w') as data_file:
 					json.dump(stt_result, data_file, indent=1)
 				with open('speech-snippets/' + basename + '_' + str(i) + '.json') as data_file:
-					good_timestamps = prune_wrong_recog(None, data_file, self.threadID * offset_amt / 1000)
+					good_timestamps = prune_wrong_recog(None, data_file, float(self.start_time)/1000, self.threadID)
 
 				#clip audio into word clips
-				extract_words(sys.argv[2], good_timestamps, sys.argv[1].lower(), i/1000)
-		print ("Exiting " + self.name)
+				extract_words(sys.argv[2], good_timestamps, i/1000, self.threadID)
 
+def ffmpeg_extract_subclip(filename, start, end, targetname=None):
+	"""
+	Creates a new video file playing video file "filename" between
+		the times "start" and "end". 
 
-def ffmpeg_extract_subclip(filename, t1, t2, targetname=None):
-	""" makes a new video file playing video file ``filename`` between
-		the times ``t1`` and ``t2``. 
+	Parameters
+	--------------------
+		filename			-- 	path
+		start 				--	time to start cutting from
+		end 				--	time to finish cutting to
+		targetname			--	output file name (not used)
+
+	Returns
+	--------------------
+		nothing
+	"""
+	""" 
 		Note: This function is from the moviepy library but it was buggy so I fixed it here"""
 	name,ext = os.path.splitext(filename)
 	if not targetname:
-		T1, T2 = [int(1000*t) for t in [t1, t2]]
+		T1, T2 = [int(1000*t) for t in [start, end]]
 		targetname = name+ "%sSUB%d_%d.%s"(name, T1, T2, ext)
 	
 	cmd = [get_setting("FFMPEG_BINARY"), "-y",
 	  "-i", filename,
-	  "-ss", "%0.2f"%t1,
-	  "-t", "%0.2f"%(t2-t1),
+	  "-ss", "%0.2f"%start,
+	  "-t", "%0.2f"%(end-start),
 	  targetname]
 	
-	subprocess_call(cmd)
+	subprocess_call(cmd, False)
 
-def prune_wrong_recog( script, data_file, offset ):
+def prune_wrong_recog( script, data_file, offset, ID ):
 	"""
 	Returns the correctly recognized words and their timestamps.
 
@@ -107,7 +119,6 @@ def prune_wrong_recog( script, data_file, offset ):
 	"""
 
 	good_timestamps = []
-
 	data = json.load(data_file)
 
 	for res in data['results']:
@@ -118,7 +129,6 @@ def prune_wrong_recog( script, data_file, offset ):
 				end_time = word_alternatives['end_time'] + offset
 				pred_word = word['word']
 				tup = (pred_word, start_time, end_time) 
-				print(tup)
 				good_timestamps.append(tup)
 
 	return good_timestamps
@@ -139,7 +149,7 @@ def assure_path_exists(path):
 	if not os.path.exists(dir):
 		os.makedirs(dir)
 
-def extract_words(orig_clip, good_timestamps, person, seconds):
+def extract_words(orig_clip, good_timestamps, seconds, ID):
 	"""
 	Extract word clips from the input audio clip.
 
@@ -147,7 +157,6 @@ def extract_words(orig_clip, good_timestamps, person, seconds):
 	--------------------
 		orig_clip			-- 	The input audio clip that we want to extract word clips from
 		good_timestamps		-- 	The timestamps of the words
-		person 				-- 	The person whose voice this is
 		seconds				--  How many seconds have passed
 
 	Returns
@@ -159,24 +168,23 @@ def extract_words(orig_clip, good_timestamps, person, seconds):
 	for cur, nxt in zip(good_timestamps, good_timestamps[1:]+[(None, float("inf"), None)]):
 		word = cur[0]
 		start =  seconds + cur[1]
-		cur_end =  seconds + cur[2]
+		end =  seconds + cur[2]
 		nxt_start =  seconds + nxt[1]
-
-		end = cur_end #if (cur_end + 0.1 < nxt_start) else nxt_start
 		
-		no_special_char = True
 		#check for special chars b/c windows doesn't allow these in filenames
+		no_special_char = True
 		for char in special_chars:
 			if char in word:
 				no_special_char = False
 				break
 
 		if no_special_char:
-			path = "clips/" + person + "/" + word.lower() + "/"
+			path = "clips/" + person + "/" + word.lower() + "/" + "thread" + str(ID) + "_"
 			assure_path_exists(path)
-			num_clips = len(glob.glob(path + '*')) # get num of clips already in folder, to avoid overwiting
-			ffmpeg_extract_subclip(orig_clip, start, end, targetname=(path  + str(num_clips + 1) + ".mp4"))
-
+			num_clips = len(glob.glob(path + "*")) # get num of clips already in folder, to avoid overwiting
+			ffmpeg_extract_subclip(orig_clip, start, end, targetname=(path + str(num_clips + 1) + ".mp4"))
+			if word == "we" and ID == 1:
+				print("start: " + str(start) + ", end: ", str(end))
 
 ######################################################################
 # main
@@ -187,12 +195,18 @@ def main(argv) :
 		print('Usage: speech2text.py person inputfile')
 		sys.exit(2)
 
+	#modifying global variables
+	global basename
+	global person
+	global clip_len
+
 	#open input file and convert to flac (assume  test file in same directory for now)
 	basename = os.path.splitext(os.path.basename(sys.argv[2]))[0]
 	file_ext = os.path.splitext(sys.argv[2])[1][1:]
 	audio_init = AudioSegment.from_file(sys.argv[2], file_ext) #assuming input files are all supported by ffmpeg
 	audio_chunk = AudioSegment.silent(duration=0) 
-	endtime=len(audio_init)
+	end_time = len(audio_init)
+	person = sys.argv[1].lower()
 
 	if os.path.exists("workspace"):
 		shutil.rmtree("workspace") #clear workspace and remake it
@@ -200,52 +214,26 @@ def main(argv) :
 	else:
 		os.makedirs("workspace")
 
-	clip_len = endtime/ num_threads
+	clip_len = end_time / num_threads
 	threads = []
+	start_time = 0
+	end = 0
+
 	for i in range(0,num_threads): 
-		if  i == num_threads -1:
-			audio_chunk = audio_init[i * clip_len: endtime ]
-		else:
-			audio_chunk = audio_init[i * clip_len: (i+1) * clip_len]
-		thread = myThread(i, audio_chunk)
+		start_time = i * clip_len
+		end = end_time if (i == num_threads -1) else (i+1) * clip_len -1
+
+		audio_chunk = audio_init[start_time: end]
+		thread = myThread(i, audio_chunk, start_time)
 		thread.start()
 		threads.append(thread)
 
 	for t in threads:
 		t.join()
 
-
-
-
-	# increment_by= 60000 * num_threads
-	# for i in range(thread.threadID, endtime, increment_by):
-
-
-	# for i in range(0,endtime, 60000): #chunk audio file into 60s segments
-	# 	if(i + 60000 > endtime):
-	# 		audio_chunk = audio_init[i:endtime]
-	# 	else:
-	# 		audio_chunk = audio_init[i:i+60000]
-	# 	audio_chunk.export("workspace/" + str(i) + ".wav", format="wav")
-
-	# 	#Use Watson Speech API on audio chunk
-	# 	with open("workspace/" + str(i) + ".wav", 'rb') as audio:
-	# 		stt.models()
-	# 		stt.get_model('en-US_BroadbandModel')
-	# 		stt_result = stt.recognize(
-	# 			audio, content_type='audio/wav', timestamps=True, word_confidence=True, continuous=True, profanity_filter=False,
-	# 			word_alternatives_threshold=0.0
-	# 		)#the parameters above can be altered to effect the output of the api
-
-	# 		#dump response to a json file if we want to check it later then open it
-	# 		with open('speech-snippets/' + basename + '_' + str(i) + '.json', 'w') as data_file:
-	# 			json.dump(stt_result, data_file, indent=1)
-	# 		with open('speech-snippets/' + basename + '_' + str(i) + '.json') as data_file:
-	# 			good_timestamps = prune_wrong_recog(None, data_file)  #TODO: add script to arguments of this file
-
-	# 		#clip audio into word clips
-	# 		extract_words(sys.argv[2], good_timestamps, sys.argv[1].lower(), i/1000)
-
+	# for root, dirs, files in os.walk("/clips/" + person + "/"):
+	# 	print files
+		# for file in glob.glob("*.txt"):
 
 if __name__ == "__main__" :
 	main(sys.argv[1:])
